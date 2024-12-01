ad502a7341271bab64af5fb1d6e9459ae1eb1d40
Kimia509 <kwabena.kimia@gmail.com>
2024-11-30 20:48:40 -0400

    added tracing 4.5.4 Instrument Futures

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,7 +1,8 @@
 //! src/routes/subscriptions.rs
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
-use sqlx::PgPool;
+use sqlx::{query, PgPool};
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -12,7 +13,7 @@ pub struct FormData {
 
 // Let's start simple: we always return a 200 OK
 pub async fn subscribe(
-    _form: web::Form<FormData>,
+    form: web::Form<FormData>,
     // Retrieving a connection from the application state!
     pool: web::Data<PgPool>,
 ) -> HttpResponse {
@@ -21,27 +22,75 @@ pub async fn subscribe(
     // We use a `match` statement to choose what to do based
     // on the outcome.
     // We will talk more about `Result` going forward!
+    // We are using the same interpolation syntax of `println`/`print` here!
+
+    // Let's generate a random unique identifier
+    let request_id = Uuid::new_v4();
+    // Spans, like logs, have an associated level
+    // `info_span` creates a span at the info-level
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    );
+    // Using `enter` in an async function is a recipe for disaster!
+    // Bear with me for now, but don't do this at home.
+    // See the following section on `Instrumenting Futures`
+    let _request_span_guard = request_span.enter();
+    // We do not call `.enter` on query_span!
+    // `.instrument` takes care of it at the right moments
+    // in the query future lifetime
+    let query_span = tracing::info_span!("Saving new subscriber details in the database.");
+    tracing::info!(
+        "request_id {} Adding '{}' '{}' as a new subscriber.",
+        request_id,
+        form.email,
+        form.name
+    );
+    // Using `enter` in an async function is a recipe for disaster!
+    // Bear with me for now, but don't do this at home.
+    // See the following section on `Instrumenting Futures`
+    let _request_span_guard = request_span.enter();
+
+    tracing::info!(
+        "request_id {} - Saving new subscriber details in the database.",
+        request_id
+    );
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES ($1, $2, $3, $4)
         "#,
         Uuid::new_v4(),
-        _form.email,
-        _form.name,
+        form.email,
+        form.name,
         Utc::now(),
     )
     // We use `get_ref` to get an immutable reference to the `PgConnection`
     // wrapped by `web::Data`.
     .execute(pool.get_ref())
+    // First we attach the instrumentation, then we `.await` it
+    .instrument(query_span)
     .await
     {
         // If the query was successful, we return a 200 OK response.
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            tracing::info!(
+                " request_id {} - New subscriber details have been saved.",
+                request_id
+            );
+            HttpResponse::Ok().finish()
+        }
         // If the query failed, we return a 400 Bad Request response.
         Err(e) => {
-            eprintln!("Failed to execute query: {:?}", e);
+            //Debug gives us that raw view, while std::fmt::Display ({})
+            // will return a nicer error message that is more suitable to be
+            // shown directly to our end users.
+            tracing::error!("request_id {} Failed to execute query: {:?}", e, request_id);
             HttpResponse::InternalServerError().finish()
         }
     }
+    // `_request_span_guard` is dropped at the end of `subscribe`
+    // That's when we "exit" the span
 }
